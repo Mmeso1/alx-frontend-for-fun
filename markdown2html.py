706a25d1f5abbea5f0c6392a09ee4e0dfe605806
Mmeso1 <anisiubammesoma8@gmail.com>
2024-06-16 00:29:22 +0100

    markdown2html

--- markdown2html.py
+++ markdown2html.py
@@ -15,26 +15,29 @@ Example:
 
 import sys
 import markdown
+import os
 
 def main():
     if len(sys.argv) != 3:
-        sys.stderr.write("Usage: ./markdown2html.py [input_file] [output_file]")
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
 
     markdown_file = sys.argv[1]
     output_file = sys.argv[2]
 
-    try:
-        with open(markdown_file, 'r') as f:
-            text = f.read()
-    except FileNotFoundError:
+    if not os.path.exists(markdown_file):
         sys.stderr.write(f"Missing '{markdown_file}'\n")
         sys.exit(1)
+
+    with open(markdown_file, 'r') as f:
+        text = f.read()
 
     html = markdown.markdown(text)
 
     with open(output_file, 'w') as f:
         f.write(html)
+
+    sys.exit(0)
 
 if __name__ == '__main__':
     main()
